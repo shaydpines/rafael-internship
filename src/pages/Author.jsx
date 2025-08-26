52e3ab66d7933f584a61730fafb13328fbfeee87
shaydpines <oceanrooster@gmail.com>
2025-08-26 00:45:19 -0400

    Author.jsx page dynamic

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const Author = () => {
+  let params = useParams();
+  const [author, setAuthor] = useState([]);
+  const [collection, setCollection] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [following, setFollowing] = useState(false);
+
+  async function getData() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${params.authorId}`
+    );
+    setAuthor(data);
+    setCollection(data.nftCollection);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    getData();
+    console.log(collection);
+  }, []);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-
         <section
           id="profile_banner"
           aria-label="section"
           className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
+          data-bgimage={`url(${collection[0]?.nftImage}) top`}
+          style={{ background: `url(${collection[0]?.nftImage}) top` }}
         ></section>
-
         <section aria-label="section">
           <div className="container">
             <div className="row">
@@ -25,15 +45,17 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={author.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {author.authorName}
+                          <span className="profile_username">
+                            @{author.tag}
+                          </span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {author.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -43,19 +65,32 @@ const Author = () => {
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
+                    {following ? (
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {author.followers + 1} followers
+                        </div>
+                        <Link to="#" className="btn-main" onClick={() => setFollowing(false)}>
+                          Unfollow
+                        </Link>
+                      </div>
+                    ) : (
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {author.followers} followers
+                        </div>
+                        <Link to="#" className="btn-main" onClick={() => setFollowing(true)}>
+                          Follow
+                        </Link>
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems author={author} collection={collection} />
                 </div>
               </div>
             </div>
