10f9619590778595db4c495db964c937a313a077
shaydpines <oceanrooster@gmail.com>
2025-08-26 10:57:54 -0400

    clearing minor changes

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import nftImage from "../images/nftImage.jpg";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { FaCheck } from "react-icons/fa";
